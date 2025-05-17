71d5827ddfa9837c2ffd8ab157e95d84012e3c48
Babatunde Alaraje <babatunde.alaraje@karsun-llc.com>
2025-05-17 06:59:28 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">Hello from Integreative LTD</h1>
+    </div>
+  );
+}
+
+export default App;
