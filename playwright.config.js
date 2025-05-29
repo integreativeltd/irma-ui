25d5540819fbb5a30fa1291792d71a97ba493640
Babatunde Alaraje <babatunde.alaraje@karsun-llc.com>
2025-05-28 19:58:44 -0500

    update the test

--- playwright.config.js
+++ playwright.config.js
@@ -4,12 +4,14 @@ export default defineConfig({
   testDir: './tests',
   testMatch: '**/*.spec.js',
   use: {
-    baseURL: 'http://localhost:3001',
+    baseURL: 'http://localhost:3000',
     trace: 'on-first-retry',
   },
   webServer: {
     command: 'npm run dev',
-    url: 'http://localhost:3001',
+    url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
+    timeout: 120000, // 2 minutes
   },
+  timeout: 30000, // 30 seconds per test
 });
